2b744d400eaf2c5b02594b1e550a6be0e8ad9592
Joe <joeogbulezie@gmail.com>
2024-12-30 03:55:40 +0100

    edit for stage

--- app.py
+++ app.py
@@ -1,11 +1,5 @@
 from flask import Flask, render_template, url_for, redirect, flash, jsonify, session, abort, send_from_directory
 from flask_bootstrap import Bootstrap5
-# from flask_sqlalchemy import SQLAlchemy
-# from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
-# from werkzeug.security import generate_password_hash, check_password_hash
-# from random import randint
-# from functools import wraps
-# from libgravatar import Gravatar
 from forms import *
 from data import Billify
 from mail import Mail
@@ -18,10 +12,6 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 
 Bootstrap5(app)
-
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# db = SQLAlchemy(app)
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -50,5 +40,4 @@ def about():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
-    # app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
