63c1b340c28ef296a588b7670dae36da28533c58
Joe <joeogbulezie@gmail.com>
2024-12-30 04:16:35 +0100

    update templates and app.py

--- forms.py
+++ forms.py
@@ -3,12 +3,14 @@ from wtforms import StringField, PasswordField, SubmitField, DateField, RadioFie
 from wtforms.validators import Length, DataRequired, Email, EqualTo
 from flask_ckeditor import CKEditorField
 
+
 class ContactForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()], render_kw={"placeholder": "Name"})
     email = StringField('Email Address', validators=[DataRequired(), Email()], render_kw={"placeholder": "Email"})
     phone = StringField('Phone Number', render_kw={"placeholder": "Phone Number"})
     message = CKEditorField('Message', validators=[DataRequired()], render_kw={"placeholder": "Message"})
     submit = SubmitField('Submit')
+
 
 class DateForm(FlaskForm):
     date = DateField('Select a Date', format='%Y-%m-%d', validators=[DataRequired()])
